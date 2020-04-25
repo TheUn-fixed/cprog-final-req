ac66d08e53bdb9b8f5d0f3d5e9f4db72e57a563f
Kinni <kinniyya19@gmail.com>
2020-04-26 02:51:46 +0800

    Initial Commit

---
+++ cprog/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cprog
+//
+//  Created by Kinni Yya Lopez on 4/26/20.
+//  Copyright © 2020 Kinni Yya Lopez. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
